ccjavadev/cc-bulletinboard-ads-spring-webmvc

Solution Exercise 18: Make Fallback configurable by its Caller by making use of Lambda expressions
- close #4

src/test/java/com/sap/bulletinboard/ads/services/GetUserCommandTest.java
@@ -3,6 +3,8 @@ package com.sap.bulletinboard.ads.services;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.util.function.Supplier;
+
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -21,35 +23,35 @@ public class GetUserCommandTest {
 
     @Test
     public void responseReturnedSynchronously() {
-        TestableUserCommand command = new TestableUserCommand().responseWithOkUser();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).responseWithOkUser();
         User user = command.execute();
         assertThat(user, is(USER));
     }
 
     @Test
     public void responseReturnedAsynchronously() throws Exception {
-        TestableUserCommand command = new TestableUserCommand().responseWithOkUser();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).responseWithOkUser();
         User user = command.queue().get();
         assertThat(user, is(USER));
     }
 
     @Test
     public void responseTimedOutFallback() {
-        TestableUserCommand command = new TestableUserCommand().provokeTimeout();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).provokeTimeout();
         User user = command.execute();
-        assertThat(user, is(not(USER)));
+        assertThat(user, is(FALLBACK_USER));
     }
 
     @Test
     public void responseErrorFallback() {
-        TestableUserCommand command = new TestableUserCommand().responseWithError();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).responseWithError();
         User user = command.execute();
-        assertThat(user, is(not(USER)));
+        assertThat(user, is(FALLBACK_USER));
     }
 
     @Test(expected = HystrixBadRequestException.class)
     public void responseHystrixBadRequest() {
-        TestableUserCommand command = new TestableUserCommand().responseWithBadRequest();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).responseWithBadRequest();
         User user = null;
         try {
             user = command.execute();
@@ -74,8 +76,8 @@ public class GetUserCommandTest {
         private ResponseEntity<User> response;
         private boolean provokeTimeout;
 
-        TestableUserCommand() {
-            super("", null);
+        TestableUserCommand(Supplier<User> fallbackFunction) {
+            super("", null, fallbackFunction);
         }
 
         TestableUserCommand responseWithOkUser() {

SAP-archive/cloud-bulletinboard-ads

src/test/java/com/sap/bulletinboard/ads/services/GetUserCommandTest.java
@@ -3,6 +3,8 @@ package com.sap.bulletinboard.ads.services;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.util.function.Supplier;
+
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
@@ -24,35 +26,35 @@ public class GetUserCommandTest {
 
     @Test
     public void responseReturnedSynchronously() {
-        TestableUserCommand command = new TestableUserCommand().respondWithOkUser();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).respondWithOkUser();
         User user = command.execute();
         assertThat(user, is(USER));
     }
 
     @Test
     public void responseReturnedAsynchronously() throws Exception {
-        TestableUserCommand command = new TestableUserCommand().respondWithOkUser();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).respondWithOkUser();
         User user = command.queue().get();
         assertThat(user, is(USER));
     }
 
     @Test
     public void responseTimedOutFallback() {
-        TestableUserCommand command = new TestableUserCommand().provokeTimeout();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).provokeTimeout();
         User user = command.execute();
-        assertThat(user, is(not(USER)));
+        assertThat(user, is(FALLBACK_USER));
     }
 
     @Test
     public void responseErrorFallback() {
-        TestableUserCommand command = new TestableUserCommand().respondWithError();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).respondWithError();
         User user = command.execute();
-        assertThat(user, is(not(USER)));
+        assertThat(user, is(FALLBACK_USER));
     }
 
     @Test(expected = HystrixBadRequestException.class)
     public void responseHystrixBadRequest() {
-        TestableUserCommand command = new TestableUserCommand().respondWithBadRequest();
+        TestableUserCommand command = new TestableUserCommand(this::dummyUser).respondWithBadRequest();
         User user = null;
         try {
             user = command.execute();
@@ -78,8 +80,8 @@ public class GetUserCommandTest {
         private boolean provokeTimeout;
         private RestClientException exception;
 
-        TestableUserCommand() {
-            super("", null);
+        TestableUserCommand(Supplier<User> fallbackFunction) {
+            super("", null, fallbackFunction);
         }
 
         TestableUserCommand respondWithOkUser() {